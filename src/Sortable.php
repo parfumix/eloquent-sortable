c336cf219a627c82b571e36ec2df6e71aa197c20
Radu <radu.bordei89@gmail.com>
2015-09-07 17:21:04 +0300

    adding contract functions.

--- src/Sortable.php
+++ src/Sortable.php
@@ -2,4 +2,23 @@
 
 namespace Eloquent\Sortable;
 
-interface Sortable {}
+use Illuminate\Database\Eloquent\Model;
+
+interface Sortable {
+
+    /**
+     * Insert model before current
+     *
+     * @param Model $model
+     * @return mixed
+     */
+    public function before(Model $model);
+
+    /**
+     * Insert model after current .
+     *
+     * @param Model $model
+     * @return mixed
+     */
+    public function after(Model $model);
+}
